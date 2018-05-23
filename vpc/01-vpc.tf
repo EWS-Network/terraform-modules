ea8f13819d83856a549adfdbb61620418c88bb1d
John Mille <john.mille@claranet.uk>
2018-05-23 16:17:00 +0100

    Leaving the provider in main and VPC in separate file

---
+++ vpc/01-vpc.tf
@@ -0,0 +1,25 @@
+################################################################################
+##
+## VPC
+
+resource "aws_vpc" "vpc_root" {
+  cidr_block = "${var.cidr}"
+
+  enable_dns_support   = "{var.enable_dns_support}"
+  enable_dns_hostnames = "{var.enable_dns_hostnames}"
+
+  instance_tenancy = "${var.instance_tenancy}"
+
+  tags {
+    Name      = "${var.vpc_name}"
+    Terraform = "Yes"
+  }
+}
+
+################################################################################
+##
+## Internet GW
+
+resource "aws_internet_gateway" "vpc_gw" {
+  vpc_id = "${aws_vpc.vpc_root.id}"
+}
