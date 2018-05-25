a933a08d720234c3c298a2fe6f92081c771eee5e
John Mille <john.mille@claranet.uk>
2018-05-25 10:51:32 +0100

    Changed location of iam_profile to GitHub

--- examples/iam/main.tf
+++ examples/iam/main.tf
@@ -2,7 +2,7 @@
 
 module "tf_iam_profile" "bastion_profile" {
 
-    source = "/home/john/terraform-modules/iam_instance_profile"
+    source = "github.com/ews-network/terraform-modules/iam_instance_profile"
 
     app_name = "hello_world"
 }
